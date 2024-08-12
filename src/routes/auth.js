8d642adb1bf24d23c391f5e5228f1f98a21b6873
Kaju Kopi <kajukopi@gmail.com>
2024-08-12 17:58:24 +0700

    as

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,7 +1,6 @@
 const express = require("express")
 const User = require("../models/User")
 const router = express.Router()
-const Joi = require("joi")
 
 // Login
 router.post("/login", async (req, res) => {
